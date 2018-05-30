c464a69bbd11bef1681ae6d8f96a364e9cdee78b
Ken Smith <k.w.smith@qub.ac.uk>
2018-05-30 18:47:36 +0100

    Got HTM library to build automatically inside Docker.

--- src/alert_stream_lasair/utils/htm/python/build.sh
+++ src/alert_stream_lasair/utils/htm/python/build.sh
@@ -5,4 +5,4 @@
 swig -c++ -python htmCircle.i
 g++ -c HTMCircleRegion.cpp -fPIC  -I../include -Wno-deprecated -Wall -c -g -m64 -O2
 g++ -c htmCircle_wrap.cxx -fPIC -I../include -Wno-deprecated  -Wall -c -g -m64 -O2 -fno-strict-aliasing `python-config --includes`
-g++ -shared  -o _htmCircle.so HTMCircleRegion.o htmCircle_wrap.o -lhtm -L../htm -L`python-config --prefix`/lib
+g++ -shared  -o _htmCircle.so HTMCircleRegion.o htmCircle_wrap.o -lhtm -L.. -L`python-config --prefix`/lib
