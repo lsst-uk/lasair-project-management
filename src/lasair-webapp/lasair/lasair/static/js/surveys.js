d1613fb4d3314330dd7da495b51a3eb05ac0b282
Roy Williams <roy@roe.ac.uk>
2019-01-17 15:40:48 +0000

    typos

---
+++ src/lasair-webapp/lasair/lasair/static/js/surveys.js
@@ -0,0 +1,178 @@
+var surveys = [
+     {
+        "id": "P/PanSTARRS/DR1/color-z-zg-g",
+        "url": "http://alasky.u-strasbg.fr/Pan-STARRS/DR1/color-z-zg-g/",
+        "name": "PanSTARRS",
+        "maxOrder": 11,
+        "frame": "equatorial",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/CONSTELLATIONS6",
+        "url": "http://darts.isas.jaxa.jp/pub/judo2/HiPS/Constellations2",
+        "name": "Constellations",
+        "maxOrder": 6,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS00101",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_0001_001",
+        "name": "2MASS galaxy density 4 to 43 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS0102",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_001_002",
+        "name": "2MASS galaxy density 43 to 85 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS0203",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_002_003",
+        "name": "2MASS galaxy density 85 to 128 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS0304",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_003_004",
+        "name": "2MASS galaxy density 128 to 170 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS0405",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_004_005",
+        "name": "2MASS galaxy density 128 to 170 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS0507",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_005_007",
+        "name": "2MASS galaxy density 170 to 300 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/1602.07710v1/HIPS071",
+        "url": "http://alasky.unistra.fr/pub/arxiv.1602.07710v1/HIPS_007_01",
+        "name": "2MASS galaxy density 300 to 420 Mpc (Antolini+)",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "png"
+     },
+     {
+        "id": "P/2MASS/color",
+        "url": "http://alasky.u-strasbg.fr/2MASS/Color",
+        "name": "2MASS colored",
+        "maxOrder": 9,
+        "frame": "equatorial",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/DSS2/color",
+        "url": "http://alasky.u-strasbg.fr/DSS/DSSColor",
+        "name": "DSS colored",
+        "maxOrder": 9,
+        "frame": "equatorial",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/DSS2/red",
+        "url": "http://alasky.u-strasbg.fr/DSS/DSS2Merged",
+        "name": "DSS2 Red (F+R)",
+        "maxOrder": 9,
+        "frame": "equatorial",
+        "format": "jpeg fits"
+     },
+     {
+        "id": "P/Fermi/color",
+        "url": "http://alasky.u-strasbg.fr/Fermi/Color",
+        "name": "Fermi color",
+        "maxOrder": 3,
+        "frame": "equatorial",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/Finkbeiner",
+        "url": "http://alasky.u-strasbg.fr/FinkbeinerHalpha",
+        "maxOrder": 3,
+        "frame": "galactic",
+        "format": "jpeg fits",
+        "name": "Halpha"
+     },
+     {
+        "id": "P/GALEXGR6/AIS/color",
+        "url": "http://alasky.u-strasbg.fr/GALEX/GR6-02-Color",
+        "name": "GALEX Allsky Imaging Survey colored",
+        "maxOrder": 8,
+        "frame": "equatorial",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/IRIS/color",
+        "url": "http://alasky.u-strasbg.fr/IRISColor",
+        "name": "IRIS colored",
+        "maxOrder": 3,
+        "frame": "galactic",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/Mellinger/color",
+        "url": "http://alasky.u-strasbg.fr/MellingerRGB",
+        "name": "Mellinger coloured",
+        "maxOrder": 4,
+        "frame": "galactic",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/SDSS9/color",
+        "url": "http://alasky.u-strasbg.fr/SDSS/DR9/color",
+        "name": "SDSS9 colored",
+        "maxOrder": 10,
+        "frame": "equatorial",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/SPITZER/color",
+        "url": "http://alasky.u-strasbg.fr/SpitzerI1I2I4color",
+        "name": "IRAC color I1,I2,I4 - (GLIMPSE, SAGE, SAGE-SMC, SINGS)",
+        "maxOrder": 9,
+        "frame": "galactic",
+        "format": "jpeg"
+     },
+     {
+        "id": "P/VTSS/Ha",
+        "url": "http://alasky.u-strasbg.fr/VTSS/Ha",
+        "maxOrder": 3,
+        "frame": "galactic",
+        "format": "png jpeg fits",
+        "name": "VTSS-Ha"
+     },
+     {
+        "id": "P/RASS",
+        "url": "http://cade.irap.omp.eu/documents/Ancillary/4Aladin/RASS/",
+        "name": "ROSAT ALL Sky Survey (X-ray)",
+        "maxOrder": 3,
+        "frame": "galactic",
+        "format": "jpeg"
+     },
+     {
+         "id": "P/allWISE/color",
+         "url": "http://alasky.u-strasbg.fr/AllWISE/RGB-W4-W2-W1/",
+         "name": "AllWISE color",
+         "maxOrder": 8,
+         "frame": "equatorial",
+         "format": "jpeg"
+     }
+];
