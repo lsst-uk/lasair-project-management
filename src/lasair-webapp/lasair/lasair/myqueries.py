a02b944486d5d7aa4f50a6d83aef31af3c3ddcc2
Roy Williams <roy@roe.ac.uk>
2019-01-30 13:25:49 +0000

    myqueries public=1 or 2

--- src/lasair-webapp/lasair/lasair/myqueries.py
+++ src/lasair-webapp/lasair/lasair/myqueries.py
@@ -14,44 +14,6 @@ def connect_db():
     return msl
 
 
-#def myqueries_home(request):
-#    message = ''
-#    if request.method == 'POST' and request.user.is_authenticated:
-#        delete      = request.POST.get('delete')
-#
-#        if delete == None:   # create new myquery
-#            name        = request.POST.get('name')
-#            description = request.POST.get('description')
-#            query       = request.POST.get('query')
-#            if request.POST.get('public') == 'on':
-#                public = 1
-#            else:
-#                public = 0
-#
-#            mq = Myqueries(user=request.user, name=name, description=description, 
-#                public=public, query=query)
-#            mq.save()
-#            message = 'Query created successfully'
-#        else:
-#            mq_id = int(delete)
-#            myquery = get_object_or_404(Myqueries, mq_id=mq_id)
-#            if request.user == myquery.user:
-#                myquery.delete()
-#                message = 'Query %s deleted successfully' % myquery.name
-
-# public myqueries belong to the anonymous user
-#    other_queries = Myqueries.objects.filter(public=1)
-#    if request.user.is_authenticated:
-#        myqueries    = Myqueries.objects.filter(user=request.user)
-#    else:
-#        myqueries    = None
-#
-#    return render(request, 'myqueries_home.html',
-#        {'myqueries': myqueries, 
-#        'other_queries': other_queries, 
-#        'authenticated': request.user.is_authenticated,
-#        'message': message})
-
 def new_myquery(request):
     is_owner = (request.user.is_authenticated)
     message = ''
@@ -96,10 +58,13 @@ def show_myquery(request, mq_id):
             myquery.name        = request.POST.get('name')
             myquery.description = request.POST.get('description')
             myquery.query       = request.POST.get('query')
-            myquery.public      = request.POST.get('public')
+            checkbox            = request.POST.get('public')
 
-            if request.POST.get('public'): myquery.public  = 1
-            else:                          myquery.public  = 0
+            if checkbox: 
+                if myquery.public == 0: 
+                    myquery.public  = 1 # if set to 1 or 2 leave it as it is
+            else:
+                myquery.public  = 0
 
             myquery.save()
             message += 'query updated'
