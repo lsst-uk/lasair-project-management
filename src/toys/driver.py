490f45ec8f32a6dc93af646151ef8626d6c8368b
Roy Williams <roy@roe.ac.uk>
2019-02-15 14:57:11 +0000

    clean code

--- src/toys/driver.py
+++ src/toys/driver.py
@@ -5,30 +5,21 @@ toys.start_again()
 print("---- Tables remade")
 
 # This is the maximum number of objects 
-n_objects = 1000
+n_objects = 1000000
 
-# Now make a lot of observations of the objects
-t = time.time()
-n_candidates = 10000
-for i in range(n_candidates):
-    toys.make_candidate(n_objects, debug=False)
-print("---- %d candidates made in %.2f seconds" % (n_candidates, time.time()-t))
-toys.print_numbers()
+def addem(n_candidates):
+    # Now make a lot of observations of the objects
+    t = time.time()
+    for i in range(n_candidates):
+        toys.make_candidate(n_objects, debug=False)
+    print("---- %d candidates made in %.2f seconds" % (n_candidates, time.time()-t))
+    toys.print_numbers()
 
-# Recompute the collective propoerties of each object
-t = time.time()
-toys.update_objects(debug=False)
-print("---- objects freshened in %.2f seconds" % (time.time()-t))
+    # Recompute the collective properties of each object
+    t = time.time()
+    toys.update_objects(debug=False)
+    print("---- objects freshened in %.2f seconds" % (time.time()-t))
 
-# Now make more observations of the objects
-t = time.time()
-n_candidates = 10000
-for i in range(n_candidates):
-    toys.make_candidate(n_objects, debug=False)
-print("---- %d candidates made in %.2f seconds" % (n_candidates, time.time()-t))
-toys.print_numbers()
-
-# Recompute the collective propoerties of each object
-t = time.time()
-toys.update_objects(debug=False)
-print("---- objects freshened in %.2f seconds" % (time.time()-t))
+addem(10000)
+addem(1000)
+addem(10000)
