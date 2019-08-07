118c0b4350f552b989dadd47edea26e3b959359e
Roy Williams <roy@roe.ac.uk>
2019-08-07 13:38:07 +0100

    tns

--- src/alert_stream_ztf/tns_log.py
+++ src/alert_stream_ztf/tns_log.py
@@ -1,17 +1,16 @@
-#!/home/roy/anaconda2/bin/python
-
 import sys, os
-sys.path.append('/home/roy/lasair/src/alert_stream_ztf/common')
 import date_nid
 import time
+import settings
 
 while 1:
     nid  = date_nid.nid_now()
     date = date_nid.nid_to_date(nid)
     logfile = '/data/ztf/logs/ztf_' + date + '_tns.log'
 
+    py = settings.LASAIR_ROOT + 'anaconda3/envs/sherlock/bin/python '
     cmd = 'date >> %s;' % logfile
-    cmd += '/home/roy/anaconda2/bin/python /home/roy/lasair/src/post_ingest/poll_tns.py --pageSize=1000 --inLastNumberOfDays=180 >> %s;' % logfile
-    cmd += '/home/roy/anaconda2/bin/python /home/roy/lasair/src/alert_stream_ztf/common/run_tns_crossmatch.py >> %s' % logfile
+    cmd += py + settings.LASAIR_ROOT + 'lasair/src/post_ingest/poll_tns.py --pageSize=1000 --inLastNumberOfDays=180 >> %s;' % logfile
+    cmd += py + settings.LASAIR_ROOT + 'lasair/src/alert_stream_ztf/common/run_tns_crossmatch.py >> %s' % logfile
     os.system(cmd)
     time.sleep(7200)
