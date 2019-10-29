001831102200dcc989f97940bf66ee0b08eccee4
Roy Williams <roy@roe.ac.uk>
2019-10-29 16:51:37 +0000

    upgrade

--- src/alert_stream_ztf/common/queries.py
+++ src/alert_stream_ztf/common/queries.py
@@ -4,36 +4,51 @@ def make_query(selected, tables, conditions, page, perpage, check_days_ago, days
 # select some quantitites from some tables
     sqlquery_real  = 'SELECT /*+ MAX_EXECUTION_TIME(300000) */ ' 
     sqlquery_real += selected
-    sqlquery_real += ' FROM ' + tables
 
 # if they added a days_ago clause, compute it here and prepent to conditions
-    toktables = [x.strip() for x in tables.split(',')]
+    toktables = []
+    wl_id = -1
+    for table in tables.split(','):
+        table = table.strip()
+        if table.startswith('watchlist'):
+            tok = table.split(':')
+            toktables.append('watchlist_hits')
+            wl_id = int(tok[1])
+        else:
+            toktables.append(table)
+
     time_conditions = []
     if check_days_ago:
         if 'objects' in toktables:
             time_conditions.append('objects.jdmax > JDNOW() - %.5f' % days_ago_objects)
         if 'candidates' in toktables:
             time_conditions.append('candidates.jd > JDNOW() - %.5f' % days_ago_candidates)
-    if len(conditions.strip()) > 0:
-        new_conditions = ' AND '.join(time_conditions + [conditions])
+
+    if wl_id >= 0:
+        wl_conditions = ['watchlist_hits.wl_id=%d' % wl_id]
     else:
-        new_conditions = ' AND '.join(time_conditions)
+        wl_conditions = []
+
+    if len(conditions.strip()) > 0:
+        new_conditions = ' AND '.join(wl_conditions + time_conditions + [conditions])
+    else:
+        new_conditions = ' AND '.join(wl_conditions + time_conditions)
 
 # list of joining conditions is prepended
     join_list = []
-    toktables = [x.strip() for x in tables.split(',')]
     if 'objects' in toktables:
-        toktables.remove('objects')
         for table in toktables:
             if table.startswith('sherlock'):
                 join_list.append('objects.primaryId = %s.transient_object_id' % table)
             else:
                 join_list.append('objects.objectId = %s.objectId' % table)
+
     if len(new_conditions.strip()) > 0:
         join_new_conditions = ' AND '.join(join_list + [new_conditions])
     else:
         join_new_conditions = ' AND '.join(join_list)
 
+    sqlquery_real += ' FROM ' + ','.join(toktables)
 # conditions may have no where clause just order by
     if join_new_conditions.strip().lower().startswith('order'):
         sqlquery_real += ' ' + join_new_conditions
