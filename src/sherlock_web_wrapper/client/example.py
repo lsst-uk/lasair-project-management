0319556e6c38b7f6881a90ad60fd394a1f1bf22d
Roy Williams <roy@roe.ac.uk>
2020-03-19 14:12:09 +0000

    update

--- src/sherlock_web_wrapper/client/example.py
+++ src/sherlock_web_wrapper/client/example.py
@@ -1,1 +1,6 @@
-print('hello')
+import requests
+json_request = open('example.json').read()
+url = 'http://lasair-dev-node0/'
+response = requests.post(url, data={'json_request': json_request})
+print(response)
+print(response.content)
